c590c0dbf3759b651812afc5e14fd585dbd7567d
Jóhann Friðriksson <johann@tern.is>
2025-05-05 17:04:42 +0000

    Fixed NVidia driver thing for johann-p1

--- hosts/johann-p1/hardware-nvidia.nix
+++ hosts/johann-p1/hardware-nvidia.nix
@@ -28,11 +28,24 @@
     open = true;
     nvidiaSettings = false;
 
+    # Whatever latest is
+    # package = config.boot.kernelPackages.nvidiaPackages.latest;
+
+    # This package broke because of struct drm_driver didn't have a 'date' member anymore.
+    # package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    #   version = "570.86.16"; # use new 570 drivers
+    #   sha256_64bit = "sha256-RWPqS7ZUJH9JEAWlfHLGdqrNlavhaR1xMyzs8lJhy9U=";
+    #   openSha256 = "sha256-DuVNA63+pJ8IB7Tw2gM4HbwlOh1bcDg2AN2mbEU9VPE=";
+    #   settingsSha256 = "sha256-9rtqh64TyhDF5fFAYiWl3oDHzKJqyOW3abpcf2iNRT8=";
+    #   usePersistenced = false;
+    # };
+
+    # This NVidia package fixes the drm_driver struct error mentioned above.
     package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "570.86.16"; # use new 570 drivers
-        sha256_64bit = "sha256-RWPqS7ZUJH9JEAWlfHLGdqrNlavhaR1xMyzs8lJhy9U=";
-      openSha256 = "sha256-DuVNA63+pJ8IB7Tw2gM4HbwlOh1bcDg2AN2mbEU9VPE=";
-      settingsSha256 = "sha256-9rtqh64TyhDF5fFAYiWl3oDHzKJqyOW3abpcf2iNRT8=";
+      version = "570.133.07";
+      sha256_64bit = "sha256-LUPmTFgb5e9VTemIixqpADfvbUX1QoTT2dztwI3E3CY=";
+      openSha256 = "sha256-9l8N83Spj0MccA8+8R1uqiXBS0Ag4JrLPjrU3TaXHnM=";
+      settingsSha256 = "sha256-XMk+FvTlGpMquM8aE8kgYK2PIEszUZD2+Zmj2OpYrzU=";
       usePersistenced = false;
     };
 
