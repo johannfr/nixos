666e4789d05fab74e308a805a1b9a414f4516480
Jóhann Friðriksson <johann@tern.is>
2026-01-29 11:45:02 +0000

    Formatting

--- hosts/johann-p1/configuration.nix
+++ hosts/johann-p1/configuration.nix
@@ -1,13 +1,14 @@
-{ config, pkgs, ... }:
-let
-  jofPackages = import ../jof-packages.nix { inherit pkgs; };
-in
 {
+  config,
+  pkgs,
+  ...
+}: let
+  jofPackages = import ../jof-packages.nix {inherit pkgs;};
+in {
   networking.hostName = "johann-p1"; # Define your hostname.
-  networking.extraHosts =
-  ''
-  192.168.199.33 isavia-tern
-  192.168.1.26 isds-a
+  networking.extraHosts = ''
+    192.168.199.33 isavia-tern
+    192.168.1.26 isds-a
   '';
 
   virtualisation.docker = {
@@ -15,29 +16,32 @@ in
     daemon.settings = {
       "bip" = "172.27.0.1/16";
       "default-address-pools" = [
-      { "base" = "172.27.0.0/16"; "size" = 24; }
+        {
+          "base" = "172.27.0.0/16";
+          "size" = 24;
+        }
       ];
     };
   };
 
   # Wireguard
   networking.firewall = {
-    allowedUDPPorts = [ 51820 10000 ];
-    allowedTCPPorts = [ 2345 ];
+    allowedUDPPorts = [51820 10000];
+    allowedTCPPorts = [2345];
   };
 
   networking.wireguard.interfaces = {
     wg0 = {
-      ips = [ "172.30.0.13/24" ];
+      ips = ["172.30.0.13/24"];
       listenPort = 51820;
       privateKeyFile = "/etc/wireguard/private";
       peers = [
         {
           publicKey = "4wPxWrFDu2q+okeMiVGbkwZfI6gbCUCrq/8XRNq5Ngk=";
-	        allowedIPs = [ "172.30.0.0/24" "172.31.0.0/24" ];
-	        endpoint = "vpn.jof.guru:51820";
-	        persistentKeepalive = 5;
-	      }
+          allowedIPs = ["172.30.0.0/24" "172.31.0.0/24"];
+          endpoint = "vpn.jof.guru:51820";
+          persistentKeepalive = 5;
+        }
       ];
     };
   };
@@ -49,7 +53,7 @@ in
   };
 
   systemd.services.fprintd = {
-    wantedBy = [ "multi-user.target" ];
+    wantedBy = ["multi-user.target"];
     serviceConfig.Type = "simple";
   };
 
@@ -58,7 +62,7 @@ in
   users.users.johann = {
     isNormalUser = true;
     description = "Johann Fridriksson";
-    extraGroups = [ "networkmanager" "wheel" "video" "render" "libvirtd" "docker" "input" "dialout" ];
+    extraGroups = ["networkmanager" "wheel" "video" "render" "libvirtd" "docker" "input" "dialout"];
     shell = pkgs.zsh;
     packages = jofPackages;
   };
@@ -69,15 +73,16 @@ in
     shell = pkgs.zsh;
   };
 
+  # Map the /opt directory so the agent can find its own files
+  systemd.tmpfiles.rules = [
+    "L+ /opt/traps - - - - /etc/profiles/per-user/root/opt/traps"
+  ];
+
   # environment.sessionVariables = rec {
   #   HYPRLAND_CONFIG = "$HOME/.config/hypr/johann-p1.conf";
   # };
 
-  programs._1password-gui.polkitPolicyOwners = [ "johann" ];
+  programs._1password-gui.polkitPolicyOwners = ["johann"];
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
-
-
-
 }
-
